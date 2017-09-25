e9c389d0a51d0b9783083c53ac5043d7b444605e
Anton Niklasson <niklasson.anton@gmail.com>
2017-09-25 22:59:59 +0200

    Moving to spaces for indentation.

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ const alfy = require('alfy');
 const lorem = require('lorem-ipsum');
 
 const sentence = lorem({
-	units: 'sentences',
-	count: 1,
-	sentenceLowerBound: 5,
+  units: 'sentences',
+  count: 1,
+  sentenceLowerBound: 5,
   sentenceUpperBound: 10
 });
 const paragraph = lorem({
-	units: 'paragraphs',
-	paragraphLowerBound: 3,
-	paragraphUpperBound: 6
+  units: 'paragraphs',
+  paragraphLowerBound: 3,
+  paragraphUpperBound: 6
 });
 
 alfy.output([
-	{
-		title: 'Sentence',
-		subtitle: 'Enter to copy the sentence',
-		arg: sentence
-	},
-	{
-		title: 'Paragraph',
-		subtitle: 'Enter to copy the paragraph',
-		arg: paragraph
-	}
+  {
+    title: 'Sentence',
+    subtitle: 'Enter to copy the sentence',
+    arg: sentence
+  },
+  {
+    title: 'Paragraph',
+    subtitle: 'Enter to copy the paragraph',
+    arg: paragraph
+  }
 ]);
