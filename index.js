351894e19b87bcd110b3dbab881b58aae005443f
Anton Niklasson <niklasson.anton@gmail.com>
2017-09-27 11:09:26 +0200

    Adding an optional count parameter.

--- index.js
+++ index.js
@@ -3,27 +3,42 @@
 const alfy = require('alfy');
 const lorem = require('lorem-ipsum');
 
-const sentence = lorem({
-  units: 'sentences',
-  count: 1,
-  sentenceLowerBound: 5,
-  sentenceUpperBound: 10
-});
-const paragraph = lorem({
-  units: 'paragraphs',
-  paragraphLowerBound: 3,
-  paragraphUpperBound: 6
-});
+const sentenceLowerBound = 5;
+const sentenceUpperBound = 10;
+const paragraphLowerBound = 3;
+const paragraphUpperBound = 6;
+
+function generateSentenceOutput(count) {
+  return {
+    title: count > 1 ? `${count} Sentences` : 'Sentence',
+    subtitle: 'Press enter to copy',
+    arg: lorem({
+      units: 'sentences',
+      count,
+      sentenceLowerBound,
+      sentenceUpperBound
+    })
+  };
+}
+
+function generateParagraphOutput(count) {
+  return {
+    title: count > 1 ? `${count} Paragraphs` : 'Paragraph',
+    subtitle: 'Press enter to copy',
+    arg: lorem({
+      units: 'paragraphs',
+      count,
+      sentenceLowerBound,
+      sentenceUpperBound,
+      paragraphLowerBound,
+      paragraphUpperBound
+    })
+  };
+}
+
+const count = parseInt(alfy.input) || 1;
 
 alfy.output([
-  {
-    title: 'Sentence',
-    subtitle: 'Enter to copy the sentence',
-    arg: sentence
-  },
-  {
-    title: 'Paragraph',
-    subtitle: 'Enter to copy the paragraph',
-    arg: paragraph
-  }
+  generateParagraphOutput(count),
+  generateSentenceOutput(count)
 ]);
